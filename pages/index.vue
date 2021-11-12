8ac087c63274827d8c62e0399e3e89f1e5f1554b
Éric Almeida <ericalmeida.sp@gmail.com>
2021-11-11 22:51:00 -0300

    page details

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,25 @@
 <script lang="ts">
 import Vue from 'vue'
 import { books } from '@/store'
+import { Book } from '@/models'
 
 export default Vue.extend({
   layout: 'ibook',
-  asyncData() {
-    console.log(books.books)
+  async asyncData() {
+    await books.index()
+  },
+  computed: {
+    $books(): Book[] {
+      return books.$all
+    }
+  },
+
+
+  methods: {
+    
+    show() {
+      console.log(this.$books)
+    }
   }
 })
 </script>
